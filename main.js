a7462308abcc72c18e47b2eb22e3563d2f032fd1
JoaquinMorais <joaquinmorais@gmail.com>
2021-10-28 09:56:30 -0300

    Navegador listo cheeeeeeeeck

--- main.js
+++ main.js
@@ -25,8 +25,33 @@ ScrollReveal().reveal('.form', {delay: 500}, {duration: 5000});
 
 window.sr = ScrollReveal();
     sr.reveal('.aptitudes-ft',{
-        duration: 4000,
+        duration: 3000,
         origin: 'left',
-        distance: '500px'
+        distance: '500px',
+        delay: 1000
     });
+    sr.reveal('.titulo-animacion',{
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    })
     
+
+
+/* NAVEGADOR */
+const icono = document.querySelector('.icono');
+const menu = document.querySelector('.navegar');
+
+console.log(menu)
+console.log(icono)
+
+icono.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread')
+        && e.target != menu && e.target != icono    ){
+            menu.classList.toggle("spread")
+    }
+}) 
